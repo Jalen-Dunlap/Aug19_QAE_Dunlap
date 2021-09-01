d484dac4e29497b65dda9c512cc5ac984cf19f66
Jalen-Dunlap <62079847+Jalen-Dunlap@users.noreply.github.com>
2021-09-01 13:14:32 -0800

    Added methods to raise and lower top and added field top

--- basics/src/main/java/basics/Convertible.java
+++ basics/src/main/java/basics/Convertible.java
@@ -1,13 +1,28 @@
 package basics;
 
 public class Convertible extends Car {
-
+	private String top = "Up";
+	
 	Convertible(String model) {
 		super(model);
 	}
 
 	public Convertible(String string, IEngine engine) {
 		super(string,engine);
+	}
+
+	public void lowerTop() {
+		System.out.println("Top was lowered.");
+		this.top = "Down";
+	}
+
+	public void raiseTop() {
+		System.out.println("Top was raised.");
+		this.top = "Up";
+	}
+
+	public String getTopStatus() {
+		return top;
 	}
 
 	
