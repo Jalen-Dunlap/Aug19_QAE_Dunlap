3c655161d7511c2267b7ffc0acdb53b44e107c91
Jalen-Dunlap <62079847+Jalen-Dunlap@users.noreply.github.com>
2021-09-01 12:12:12 -0800

    Small and Large Engine classes implement new functionality of IEngine interface

--- basics/src/main/java/basics/LargeEngine.java
+++ basics/src/main/java/basics/LargeEngine.java
@@ -1,8 +1,13 @@
 package basics;
 
 public class LargeEngine implements IEngine{
+	private final int cylinderCount = 8;
 	
 	public void start() {
 		System.out.println("Engine Started.");
+	}
+
+	public int getCylinderCount() {
+		return this.cylinderCount;
 	}
 }
--- basics/src/main/java/basics/SmallEngine.java
+++ basics/src/main/java/basics/SmallEngine.java
@@ -2,6 +2,7 @@ package basics;
 
 public class SmallEngine implements IEngine{
 	private final int cylinderCount = 4;
+	
 	public void start() {
 		
 	}
