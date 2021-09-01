a101c699d7116a360e65c9eb92062ef1a14bac06
Jalen-Dunlap <62079847+Jalen-Dunlap@users.noreply.github.com>
2021-09-01 12:09:52 -0800

    Created SmallEngine class implementing IEngine

---
+++ basics/src/main/java/basics/SmallEngine.java
@@ -0,0 +1,12 @@
+package basics;
+
+public class SmallEngine implements IEngine{
+	private final int cylinderCount = 4;
+	public void start() {
+		
+	}
+	public int getCylinderCount() {
+		return cylinderCount;
+	}
+
+}
