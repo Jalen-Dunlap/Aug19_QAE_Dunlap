3215544370a2b005f47ef36ff00f01578f24b354
Jalen-Dunlap <62079847+Jalen-Dunlap@users.noreply.github.com>
2021-09-01 13:00:21 -0800

    Added a start message for starting a small engine

--- basics/src/main/java/basics/SmallEngine.java
+++ basics/src/main/java/basics/SmallEngine.java
@@ -4,7 +4,7 @@ public class SmallEngine implements IEngine{
 	private final int cylinderCount = 4;
 	
 	public void start() {
-		
+		System.out.println("Small Engine Started");
 	}
 	public int getCylinderCount() {
 		return cylinderCount;
