8f5f00d605b83ce1af3d27313dcc7f4f7f16a311
Jalen-Dunlap <62079847+Jalen-Dunlap@users.noreply.github.com>
2021-09-01 13:16:44 -0800

    Fixed spacing issues and indicated method overrides

--- basics/src/main/java/basics/Bike.java
+++ basics/src/main/java/basics/Bike.java
@@ -3,10 +3,12 @@ package basics;
 public class Bike extends Vehicle {
 	protected int odometer = 0;
 	
+	@Override
 	public void drive(int distance) {
 		this.odometer += distance;
 	}
 
+	@Override
 	public int getOdometer() {
 		return this.odometer;
 	}
--- basics/src/main/java/basics/Convertible.java
+++ basics/src/main/java/basics/Convertible.java
@@ -24,6 +24,4 @@ public class Convertible extends Car {
 	public String getTopStatus() {
 		return top;
 	}
-
-	
 }
