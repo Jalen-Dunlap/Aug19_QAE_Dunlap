d0f342dcffdb3e63b5595692d27329365ad434af
Jalen-Dunlap <62079847+Jalen-Dunlap@users.noreply.github.com>
2021-09-11 09:24:51 -0800

    Created second test

--- basics/src/test/java/basics/SeleniumTests.java
+++ basics/src/test/java/basics/SeleniumTests.java
@@ -3,15 +3,20 @@ package basics;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class SeleniumTests {
-
+	final String WEB_DRIVER_SYSTEM_PROPERTY = "webdriver.chrome.driver";
+	
 	@Test
 	public void canLaunchWebDriver() {
 		String chromeDriverPath = "C:\\\\Users\\jjina\\Desktop\\Quintrix_Training\\chromedriver.exe";
-		System.setProperty("webdriver.chrome.driver",chromeDriverPath);
+		System.setProperty(WEB_DRIVER_SYSTEM_PROPERTY,chromeDriverPath);
 		WebDriver driver = new ChromeDriver();
 		String url = "https://www.selenium.dev/";
 		
@@ -19,5 +24,28 @@ public class SeleniumTests {
 		String currentUrl = driver.getCurrentUrl();
 		
 		assertTrue("Did not navigate to the correct url", currentUrl.equals(url));
+		
+		driver.quit();
 	}	
+	
+	@Test
+	public void canShowExplicitWait() {
+		String chromeDriverPath = "C:\\\\Users\\jjina\\Desktop\\Quintrix_Training\\chromedriver.exe";
+		System.setProperty(WEB_DRIVER_SYSTEM_PROPERTY, chromeDriverPath);
+		String baseUrl = "https://www.selenium.dev/";
+		String documentationPageUrl = baseUrl + "documentation/webdriver/";
+		long elementToBeClickableTimeoutSeconds = 5;
+		
+		WebDriver driver = new ChromeDriver();
+		driver.navigate().to(baseUrl);
+		WebElement button = driver.findElement(By.xpath("//h4[text()='Selenium WebDriver']/ancestor::div[@class='card h-100 border-0 bg-transparent']//a"));
+		WebDriverWait wait = new WebDriverWait(driver, elementToBeClickableTimeoutSeconds);
+		wait.until(ExpectedConditions.elementToBeClickable(button));
+		button.click();
+		String currentUrl = driver.getCurrentUrl();
+		
+		assertTrue("Did not navigate to the url " + documentationPageUrl, currentUrl.equals(documentationPageUrl));
+	
+		driver.quit();
+	}
 }
