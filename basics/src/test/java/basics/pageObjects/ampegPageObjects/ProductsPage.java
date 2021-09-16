3bab1e485e2a22cc9768e18c697780b5a7c13bd7
Jalen-Dunlap <62079847+Jalen-Dunlap@users.noreply.github.com>
2021-09-15 23:13:22 -0800

    Added click on Pro Series Bass Heads Product method to Products Page

--- basics/src/test/java/basics/pageObjects/ampegPageObjects/ProductsPage.java
+++ basics/src/test/java/basics/pageObjects/ampegPageObjects/ProductsPage.java
@@ -1,6 +1,8 @@
 package basics.pageObjects.ampegPageObjects;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 import basics.pageObjects.PageObject;
 
@@ -11,6 +13,8 @@ public class ProductsPage extends PageObject {
 	}
 
 	public SvtProSeriesProductPage clickSvtProSeriesBassHeadsProduct() {
+		WebElement proSeries = this.driver.findElement(By.xpath("//table[@id='tableMain']//td[@id='productIndex']//img[@alt='SVT Pro Series: Bass heads']/.."));
+		proSeries.click();
 		
 		return new SvtProSeriesProductPage(this.driver,this.baseUrl);
 	}
