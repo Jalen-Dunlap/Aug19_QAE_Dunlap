521385ed791d9c5255121b6a55942f088285241e
Jalen-Dunlap <62079847+Jalen-Dunlap@users.noreply.github.com>
2021-09-15 21:36:57 -0800

    Creation of Exercise 1 test and abstract testBase and Page Object Class

---
+++ basics/src/test/java/basics/AmpegHomePage.java
@@ -0,0 +1,13 @@
+package basics;
+
+import org.openqa.selenium.WebDriver;
+
+import basics.pageObjects.PageObject;
+
+public class AmpegHomePage extends PageObject {
+
+	protected AmpegHomePage(WebDriver driver, String baseUrl) {
+		super(driver, baseUrl);
+	}
+
+}
---
+++ basics/src/test/java/basics/PageObjectPatternProject.java
@@ -0,0 +1,33 @@
+package basics;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PageObjectPatternProject extends TestBase{
+
+	@Test
+	public void sanityTest() {
+		String currentUrl = this.driver.getCurrentUrl();
+		String expectedUrl = "https://ampeg.com/index.html";
+		
+		assertTrue("Did not reach correct website",currentUrl.equals(expectedUrl));
+	}
+	
+	@Test
+	public void canNavigateToHomePageFromProductDetailsPage() {
+		String homeUrl = this.baseUrl + "/index.html";
+		
+		AmpegHomePage page = new AmpegHomePage()
+				.clickProductsLinkInMenu()
+				.clickSvtProSeriesBassHeadsProduct()
+				.clickSvt3ProLink()
+				.clickHomeBreadcrumbLink();
+		
+		String currentUrl = this.driver.getCurrentUrl();
+		
+		assertTrue("The end page is not the home page url of: " + homeUrl, currentUrl .equals(homeUrl));
+		
+	}
+
+}
