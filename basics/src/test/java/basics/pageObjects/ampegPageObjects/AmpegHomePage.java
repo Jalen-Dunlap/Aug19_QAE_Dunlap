1d65aaa71e3a6c63b3e7743d28bc187a98db0ee9
Jalen-Dunlap <62079847+Jalen-Dunlap@users.noreply.github.com>
2021-09-15 22:09:18 -0800

    Added functionality of clicking products link on home page

--- basics/src/test/java/basics/pageObjects/ampegPageObjects/AmpegHomePage.java
+++ basics/src/test/java/basics/pageObjects/ampegPageObjects/AmpegHomePage.java
@@ -1,6 +1,8 @@
 package basics.pageObjects.ampegPageObjects;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 import basics.pageObjects.PageObject;
 
@@ -11,6 +13,9 @@ public class AmpegHomePage extends PageObject {
 	}
 
 	public ProductsPage clickProductsLinkInMenu() {
+		WebElement productsLink = this.driver.findElement(By.xpath("//div[@class='header-nav']//a[@title='Products']"));
+		productsLink.click();
+		
 		return new ProductsPage(this.driver,this.baseUrl);
 	}
 }
