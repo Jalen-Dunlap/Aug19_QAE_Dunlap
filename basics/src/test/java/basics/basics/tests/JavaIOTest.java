6b33f38084c072732cabeb7bf10ddc85f689e7b8
Jalen Dunlap <62079847+Jalen-Dunlap@users.noreply.github.com>
2021-09-27 11:07:03 -0800

    Create Test Cases

---
+++ basics/src/test/java/basics/basics/tests/JavaIOTest.java
@@ -0,0 +1,32 @@
+package basics.basics.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class JavaIOTest {
+	
+	@Test
+	public void canGetBrowserTypeFromConfig() {
+		//create string
+		//get string from config
+		//assert strings are equal to "chrome"
+		
+	}
+
+	@Test
+	public void canReadCsvFileIntoHashMap() {
+		/*
+		 * //arrange
+		 * create hashMap
+		 * 
+		 * //act
+		 * read csv file basses.csv
+		 * insert data into hashmap ignoring string count
+		 * 
+		 * //assert
+		 * assert that 
+		 */
+	}
+
+}
