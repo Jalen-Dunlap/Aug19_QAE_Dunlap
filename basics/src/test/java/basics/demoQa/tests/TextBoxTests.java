2afde0f2da6b943bf2485f27cd918353e55d93ef
Jalen Dunlap <62079847+Jalen-Dunlap@users.noreply.github.com>
2021-09-27 12:21:31 -0800

    Wrote manual test for text box test

--- basics/src/test/java/basics/demoQa/tests/TextBoxTests.java
+++ basics/src/test/java/basics/demoQa/tests/TextBoxTests.java
@@ -2,6 +2,7 @@ package basics.demoQa.tests;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import basics.demoQa.foundation.DemoQaTests;
@@ -9,8 +10,19 @@ import basics.demoQa.foundation.DemoQaTests;
 public class TextBoxTests extends DemoQaTests{
 
 	@Test
+	/*	Manual Test
+	 *  Go to /text-box
+	 *  Enter Full Name
+	 *  Enter Email
+	 *  Enter Current Address
+	 *  Enter Permanent Address
+	 *  Confirm the labels match the entered information 
+	 */
 	public void canFillOutPageForm() {
-		
 	}
 
+	@Before
+	public void setup() {
+		goTo("/text-box");
+	}
 }
